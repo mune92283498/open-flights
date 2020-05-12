37507c671e03bd6ff8c5006de5256218d895fa94
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2020-05-12 23:15:15 +0900

    fast_jsonapiのGemを追加。serializersのディレクトリを追加。reviewのモデルを追加。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+airlines = Airline.create([
+  {
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.anazonaws.com/United_Airlines.png"
+  }
+])
+
+reviews = Review.create([
+  {
+    title: "Great airline",
+    description: "I had a lovely time.",
+    score: 5,
+    airline: airlines.first
+  },
+  {
+    title: "Bad airline",
+    description: "I had a bad time.",
+    score: 1,
+    airline: airlines.first
+  }
+])
