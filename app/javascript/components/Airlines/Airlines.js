fd925d5f12b5461122491ec0d7850ab575605359
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2020-05-19 00:06:51 +0900

    axionを追加

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { setState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
-  return <div>This is the Airlines#index view for our app.</div>
+  const [airlines, setAirlines] = useStatus([]);
+
+  useEffect(() => {
+    // 全てのairlinsをAPIから取得する
+    // stateが更新されたらairlinesを更新する
+
+    axios
+      .get("/api/v1/airlines.json")
+      .then(resp => {
+        setAirlines(resp.data.data)
+      })
+      .catch(resp => console.log(resp));
+  }, [airlines.length]);
+
+  const list = airlines.map(item => {
+    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  })
+
+  return (
+    <Fragment>
+      <div>This is the Airlines#index view for our app.</div>
+      <ul>{list}</ul>
+    </Fragment>
+  );
 }
 
 export default Airlines
